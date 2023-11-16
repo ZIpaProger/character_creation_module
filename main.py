c8314c3a2edcce2644a0fc5c866c0bccbde25b50
Геннадий Кащенко <kashchenko.gena@yandex.ru>
2023-11-17 00:28:58 +0300

    Add __name__ & __main__ construction for execution module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
 
@@ -126,7 +127,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+
+    run_screensaver()
 
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -140,6 +143,3 @@ def main() -> str:
 
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
